450d0b4efdbc86be58ee33cdc5e8f9d473a6f027
Nabin Purbey <nabinpurbey03@hotmail.com>
2023-10-08 22:04:17 +0545

    initial commit

---
+++ src/Solution7.java
@@ -0,0 +1,19 @@
+public class Solution7 {
+    static int reverse(int x) {
+        int n = 0;
+        while (x != 0) {
+            int rem = x % 10;
+            x /= 10;
+            if (n > Integer.MAX_VALUE / 10 || n < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+            n = (n * 10) + rem;
+        }
+        return n;
+    }
+
+    public static void main(String[] args) {
+        int n = -123;
+        System.out.println(reverse(n));
+    }
+}

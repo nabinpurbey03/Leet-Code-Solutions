3d44ae9ba1a805dec275920fa18cf482d90b908b
Nabin Purbey <nabinpurbey03@hotmail.com>
2023-10-11 22:28:13 +0545

    initial commit

--- src/easy/Solution136.java
+++ src/easy/Solution136.java
@@ -1,18 +1,16 @@
 package easy;
 
-import java.util.Arrays;
-
 public class Solution136 {
     static int singleNumber(int[] nums) {
-        int ans = 0; //since XOR with 0 returns same number
-        for (int i = 0; i < nums.length; i++) {
-            ans ^= nums[i];  // ans = (ans) XOR (array element at i)
+        int n = 0;
+        for (int num : nums) {
+            n ^= num;
         }
-        return ans;
+        return n;
     }
 
     public static void main(String[] args) {
-        int[] n = {1,1,2,2,3,3,5};
+        int[] n = {1, 1, 2, 2, 3, 3, 5};
         System.out.println(singleNumber(n));
 
     }
